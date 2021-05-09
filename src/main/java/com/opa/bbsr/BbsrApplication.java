f00f75e4580706cc752cbf2d6799296597f43413
Shubhranshu Rauta <shrauta@microsoft.com>
2021-05-09 10:34:56 +0530

    Initial commit.

---
+++ src/main/java/com/opa/bbsr/BbsrApplication.java
@@ -0,0 +1,13 @@
+package com.opa.bbsr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbsrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opa/bbsr/BbsrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opa.bbsr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
